abdbd9b89f02ff0cbfce575cd06f192ca03f80c1
azimjonjalilov <azimjonjalilov1301@mail.ru>
2025-06-22 00:18:22 +0500

    navbar component is done

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,5 +1,40 @@
+import { links } from "../../Data";
+import { NavLink } from "react-router";
+import { RiCloseLine } from "react-icons/ri";
+import { RiMenuLine } from "react-icons/ri";
+import "./navbar.css";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className="nav">
+      <div className="nav-menu">
+        <ul className="nav-list grid">
+          {links.map(({ name, icon, path }, index) => {
+            return (
+              <li className="nav-item" key={index}>
+                <NavLink
+                  to={path}
+                  className={({ isActive }) =>
+                    isActive ? "nav-link active-nav" : "nav-link"
+                  }
+                >
+                  {icon}
+
+                  <h3 className="nav-name">{name}</h3>
+                </NavLink>
+              </li>
+            );
+          })}
+        </ul>
+
+        <RiCloseLine className="nav-close" />
+      </div>
+
+      <div className="nav-toggle">
+        <RiMenuLine />
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
